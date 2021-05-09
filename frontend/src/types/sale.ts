a4e89da012c59b2f42f6eb3c8ab280d8c97d6521
Barbozha <barbozha@gmail.com>
2021-05-09 18:16:02 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,26 @@
+import { Saller } from "./seller";
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Saller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size?: number;
+    number: number;
+    numberOfElements?: number;
+    first: boolean;
+    empty?: boolean;
+}
+
 export type SaleSum = {
     sellerName: string;
     sum: number;
